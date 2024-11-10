d2c92e60bb7a739a11684062cbc9f8e6d5154a01
saadikashaikh <shaikhsaadika@gmail.com>
2024-11-10 17:29:29 +0000

    Pending changes exported from your codespace

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,26 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/Aboutbg.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Beige abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  " I’m a recent BCA Graduate with a strong foundation in front-end technologies like HTML, CSS, JavaScript, and Bootstrap. I enjoy bringing digital designs to life with seamless and interactive user interfaces and am driven by the pursuit of innovative solutions that enhance web accessibility and user experience.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "HTML, CSS, Bootstrap",
+  "JavaScript, Node.js, Express.js",
+  "Responsive design",
+  "WordPress",
+  "SEO fundamentals",
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about exploring new ways to solve problems through technology, whether it’s optimizing a site for search engines, building with JavaScript frameworks, or ensuring smooth, responsive layouts. My goal is to combine creativity with technical skill to deliver impactful and user-friendly web solutions.";
 
 const About = () => {
   return (
